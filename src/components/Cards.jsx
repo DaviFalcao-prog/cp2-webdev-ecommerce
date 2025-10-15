2dfc033a590ae00e86dfe9adf9f488e2e404a1b9
DaviFalcao-prog <davifalcao1609@outlook.com>
2025-10-14 22:45:40 -0300

    CONSEGUII PORRA

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,9 @@
-export default function Cards(){
-    
+export default function Cards({title, image, price}){
+    return(
+        <div className="bg-blue-400">
+            <h1>{title}</h1>
+            <img src={image} alt="imagem de alguma coisa" />
+            <p>{price}</p>
+        </div>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,48 @@
-
+import Section from "../components/Section";
+import Cards from "../components/Cards";
+import { useEffect, useState } from "react";
 export default function Home() {
-  
+    const [titulo, setTitulo] = useState([]);
+    const [preco, setPreco] = useState([]);
+    const [image, setImage] = useState([]);
+
+      const API = import.meta.env.VITE_API_URL;
+     
+  useEffect(() => {
+    fetch(`${API}/jewelery`)
+      .then(res => res.json())
+      .then(data => setTitulo(data))
+  }, [])
+      useEffect(() => {
+    fetch(`${API}/men's%20clothing`)
+      .then(res => res.json())
+      .then(data => setPreco(data))
+  }, [])
+    useEffect(() => {
+    fetch(`${API}/electronics`)
+      .then(res => res.json())
+      .then(data => setImage(data))
+  }, [])
+
   return (
     <div>
         <h2 className="text-2xl font-bold mb-4">Bem-vindo ao Fiap Commerce!</h2>
-        <section titulo="">
+        <Section titulo="Joias">
+          {titulo.map(pegaItem => (
+          <Cards {...pegaItem} />
+          ))}
+        </Section>
+        <Section titulo="Preco">
+          {preco.map(pegaItem => (
+          <Cards {...pegaItem} />
+          ))}
 
-        </section>
-        <section titulo="">
-
-        </section>
-        <section titulo="">
-
-        </section>
+        </Section>
+        <Section titulo="Roupas Masculinas">
+          {image.map(pegaItem => (
+          <Cards {...pegaItem} />
+        ))}
+        </Section>
     </div>
   );
 }
